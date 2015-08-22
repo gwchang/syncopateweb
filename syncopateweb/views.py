6f7ae084ce76db6d853b4e8a170c8658032f0ad9
Gary Chang <gwh.chang@gmail.com>
2015-08-22 20:16:34 +0800

    template working

--- syncopateweb/views.py
+++ syncopateweb/views.py
@@ -3,7 +3,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def showcluster(request, url):
+    url = url.replace('/','')
     print(url)
-    context = { 'urlpath' : url }
+    keys = url.split('&')
+    serieskeys = []
+    for k in keys:
+        tokens = k.split('.')
+        if len(tokens) >= 2:
+            serieskeys.append(tokens[1])
+    wspath = '&'.join([ 'series=%s' % k for k in keys ])
+    wshost = 'api.blub.io:32790'
+    wsurl = "ws://%s/ws?%s" % (wshost, wspath);
+    print(wsurl)
+    context = { 
+        'urlpath' : url,
+        'wsurl'   : wsurl,
+        'serieskeys' : serieskeys,
+        }
     #return HttpResponse(url)
     return render(request, 'syncopateweb/index.html', context)
